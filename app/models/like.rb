cab5a5bb5bd66ec13d45a501ad95a43dd1e7dfb1
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-26 11:36:58 -0300

    created methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+
+  def update_likes_counter
+    update(likes_counter: likes.count)
+  end
 end
