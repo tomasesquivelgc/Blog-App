9f003a98a03bb8b550ae196d6456e85aab2e4d00
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-26 18:16:16 -0300

    add after_create and after_destroy methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
 
+  after_save :update_user_posts_counter
+  after_destroy :update_user_posts_counter
+
   def update_user_posts_counter
     author.update(post_counter: author.posts.count)
   end
