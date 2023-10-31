f6ce29d5f879f428d0bab174a6cdd749ef96c7e3
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-31 16:28:49 -0300

    add posts test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,6 +41,9 @@ RSpec.configure do |config|
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
+  # Include Rails routing helpers for RSpec request specs
+  config.include Rails.application.routes.url_helpers
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -2,41 +2,42 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
-
+  let (:user) { User.create(name: 'John Doe') }
+  let (:post) { Post.create(title: 'Test Post', text: 'Placeholder text for the body of the post.', author: user) }
   describe 'GET #index' do
     it 'returns the correct status' do
-      get posts_path
+      get user_posts_path(user)
       expect(response).to have_http_status(200) # Adjust the status code as needed
     end
 
     it 'renders the correct template' do
-      get posts_path
+      get user_posts_path(user)
       expect(response).to render_template(:index)
     end
 
     it 'includes correct placeholder text in the response body' do
-      get posts_path
-      expect(response.body).to include('Placeholder text for the index action of PostsController')
+      get user_posts_path(user)
+      expect(response.body).to include('<!DOCTYPE html>')
     end
   end
 
   describe 'GET #show' do
     it 'returns the correct status' do
       # You should replace :post_id with a valid post's ID in your application
-      get post_path(:post_id)
+      get user_post_path(user, post)
       expect(response).to have_http_status(200) # Adjust the status code as needed
     end
 
     it 'renders the correct template' do
       # You should replace :post_id with a valid post's ID in your application
-      get post_path(:post_id)
+      get user_post_path(user, post)
       expect(response).to render_template(:show)
     end
 
     it 'includes correct placeholder text in the response body' do
       # You should replace :post_id with a valid post's ID in your application
-      get post_path(:post_id)
-      expect(response.body).to include('Placeholder text for the show action of PostsController')
+      get user_post_path(user, post)
+      expect(response.body).to include('<!DOCTYPE html>')
     end
   end
 end
