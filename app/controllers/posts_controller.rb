9e52f72d1bf087a38ec28088fa1f0ab91981d8b3
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-11-03 14:54:34 -0300

    add empty posts message, post/:post_id template, back to main page link

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,8 @@ class PostsController < ApplicationController
   end
 
   def show
-  
+  @user = User.find(params[:user_id])
+  @post = @user.posts.find(params[:id])
+  @comments = @post.comments.all
   end
 end
