eaff4d83147a7c19ff768b69aec4c3008f8a23f4
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-11-04 17:27:24 -0300

    create current_user method and modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:create, :new]
+      resources :likes, only: [:create]
   end
 end
