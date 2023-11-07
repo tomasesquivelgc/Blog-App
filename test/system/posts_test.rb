28060b62017a8a0adf3c919c9c42c69374e4139a
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-11-07 17:25:36 -0300

    add image and comments test for posts/index

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -8,39 +8,24 @@ class PostsTest < ApplicationSystemTestCase
 
   test 'visiting the index' do
     visit user_posts_url(@user)
-
-    assert_selector "img[src='#{@user.photo}']"
-    assert_selector 'a', text: @user.name
-    assert_selector 'p', text: "Number of Posts: #{@user.post_counter}"
-
-    # Test to see if all posts are displayed
+  
+    assert_selector 'img', class: 'user-image'
+    assert_selector 'h2', text: @user.name
+    assert_selector 'p', text: "Number of posts: #{@user.post_counter}"
+  
+    # Test to see if all posts are displayed on the index page
     @user.posts.order(created_at: :desc).each do |post|
-      within("#post-#{post.id}") do
-        assert_selector 'a', text: "Post ##{post.id}"
-        assert_selector 'p', text: post.text
-        assert_selector 'span', text: "Comments: #{post.comments_counter}"
-        assert_selector 'span', text: "Likes: #{post.likes_counter}"
-        click_on "Post ##{post.id}"
-      end
+      assert_selector '.user-post-title', text: post.title
+      assert_selector '.user-post-text', text: post.text
+      assert_selector '.counter', text: "Comments: #{post.comments_counter || 0}, Likes: #{post.likes_counter || 0}"
+      assert_selector '.comments-card', text: post.recent_comments.reverse.first.text
+      click_on post.title
+  
+      # Check that you are now on the show page for the post
       assert_current_path user_post_path(@user, post)
       click_on 'Back to Post index'
     end
   end
+  
 
-  test 'visiting the show' do
-    visit user_post_url(@user, @post)
-
-    assert_selector 'h3', text: @post.id
-    assert_selector 'p', text: @post.text
-    assert_selector 'p', text: "Likes: #{@post.likes_counter}"
-    click_on 'Like'
-
-    # Test to see if all comments are displayed
-    @post.comments.order(created_at: :desc).each do |comment|
-      within('.comments-container') do
-        assert_selector 'p', text: "Username: #{comment.user.name}, #{comment.text}"
-      end
-      assert_current_path user_post_path(@user, @post)
-    end
-  end
 end
